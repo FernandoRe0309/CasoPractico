925566a37b34330f3ef3ab4bc04f043772802a9d
Fernando Rivas <fernanriv64@gmail.com>
2026-09-06 11:58:44 -0600

    Agregamos el archivo formulario.js

---
+++ scripts/formulario.js
@@ -0,0 +1,41 @@
+// Este módulo escucha el envío del formulario, arma un nuevo
+// comunicado (detectando el tipo por la extensión del archivo)
+// y lo agrega a los datos
+
+import { comunicados } from "./data.js";
+import { aplicarFiltros } from "./filtros.js";
+
+const formComunicado = document.getElementById("form-comunicado");
+const inputTitulo = document.getElementById("input-titulo");
+const inputFecha = document.getElementById("input-fecha");
+const inputFormato = document.getElementById("input-formato");
+
+//Detecta el tipo de comunicación según la extensión del archivo
+function detectarTipoPorArchivo(nombreArchivo) {
+  const extension = nombreArchivo.split(".").pop().toLowerCase();//convierte el nombre del archivo en un array por cada punto
+    //.pop agarra el ultimop elemento de ese array osea la extension
+  if (extension === "pdf") return "PDF";
+  if (extension === "xls" || extension === "xlsx") return "Excel";
+  if (extension === "doc" || extension === "docx") return "Word";
+
+  return "Desconocido";
+}
+//guarda un nuevo comunicado
+formComunicado.addEventListener("submit", (evento) => {
+  evento.preventDefault();
+
+  const archivo = inputFormato.files[0];
+  const nombreArchivo = archivo ? archivo.name : "sin_archivo";
+
+  const nuevoComunicado = {
+    titulo: inputTitulo.value.trim(),
+    fecha: inputFecha.value,
+    tipo: detectarTipoPorArchivo(nombreArchivo),
+    archivo: nombreArchivo
+  };
+
+  comunicados.push(nuevoComunicado);
+
+  formComunicado.reset();
+  aplicarFiltros();
+});
