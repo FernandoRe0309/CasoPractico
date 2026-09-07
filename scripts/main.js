95f9e39f7c5878f7620132815dc4f1466b2f5ca2
FernandoRe0309 <166575455+FernandoRe0309@users.noreply.github.com>
2026-09-06 20:36:18 -0600

    Update comment for initial page render
    Removed comment about initial render with three messages.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 import "./formulario.js";
 import { aplicarFiltros } from "./filtros.js";
 
-// Primer render al cargar la página, con los 3 comunicados iniciales
+// Primer render al cargar la página
 aplicarFiltros();
